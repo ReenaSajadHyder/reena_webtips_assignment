411a7aecc7a37c9e328309bf7badf16bf9344c7e
Reena Sajad <reena.sajad@solitontech.com>
2023-02-27 18:15:21 +0530

    Refactor: Use express framework to handle client requests

--- server.js
+++ server.js
@@ -1,73 +1,55 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
-const Port = 5000;
-
+const express = require("express");
 const {
   allTimeZones,
   timeForOneCity,
   nextNhoursWeather,
 } = require("./timeZone");
-let currentCity = "";
+const app = express();
+const Port = 5000;
 
+app.use(express.static("public"))
+app.use(express.json())
 
-var appServer = http.createServer((req, res) => {
-  let file = path.join(req.url === "/" ? "index.html" : req.url.slice(1));
-  let contentType = "text/html";
-  let extension = path.extname(file);
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".png":
-      contentType = "image/x-png";
-      break;
-    case ".svg":
-      contentType = "image/svg+xml";
-      break;
+app.get("/", (req, res) => {
+  console.log(req.url);
+  res.render("/public/index.html");
+})
+
+app.get("/allCities", (req, res) => {
+    weatherResult = allTimeZones();
+    res.json(weatherResult);
+})
+
+app.get("/cityData", (req, res) => {
+  var city = req.query.city; 
+  if(city) {
+    res.json(timeForOneCity(city));
   }
-
-  if (req.url === ".ico") {
-    res.end();
-    return;
-  } else if (req.url == "/allCities") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(allTimeZones()));
-    res.end();
-  } else if (req.url.match("/cityData")) {
-    currentCity = req.url.split("=")[1];
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(timeForOneCity(currentCity)));
-    res.end();
-  } else if (req.url.match("/nextFiveHours")) {
-    let body = " ";
-    req.on("data", (data) => (body += data.toString()));
-    req.on("end", () => {
-      let currentCityDetails = JSON.parse(body);
-      res.write(
-        JSON.stringify(
-          nextNhoursWeather(
-            currentCityDetails.city_Date_Time_Name,
-            currentCityDetails.hours,
-            allTimeZones()
-          )
-        )
-      );
-      res.end();
-    });
-  } else {
-    res.writeHead(200, { "Content-Type": contentType });
-    fs.createReadStream(file).pipe(res);
+  else{
+    res
+    .status(404)
+    .json({Error: "Not a valid endpoint. Please check API Doc"});
   }
 });
 
-appServer.listen(Port, (err) => {
-  if (err) {
+app.post("/nextFiveHours", (req, res) => {
+  let cityDTN = req.body.city_Date_Time_Name;
+  let hours = req.body.hours;  
+  if (cityDTN && hours) {
+    res.json(nextNhoursWeather(cityDTN, hours, weatherResult))
+  }
+  else{
+    res
+    .status(404)
+    .json({Error: "Not a valid endpoint. Please check API Doc"})
+  }
+});
+
+app.listen(Port, (err) => {
+  if(err){
     console.log(err);
-  } else {
+  }
+  else{
     console.log("Server connected at port number 5000 with url http://localhost:5000/");
   }
 });
