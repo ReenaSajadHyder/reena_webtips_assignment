5a05ca6a855ea59c226586a2a2aaedfddef9a038
Reena Sajad <reena.sajad@solitontech.com>
2023-03-02 15:08:58 +0530

    Refactor: Apply prettier to all the files

--- server.js
+++ server.js
@@ -7,48 +7,47 @@ const {
 const app = express();
 const Port = 5000;
 
-app.use(express.static("public"))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("/public/index.html");
-})
+});
 
 app.get("/allCities", (req, res) => {
-    weatherResult = allTimeZones();
-    res.json(weatherResult);
-})
+  weatherResult = allTimeZones();
+  res.json(weatherResult);
+});
 
 app.get("/cityData", (req, res) => {
-  var city = req.query.city; 
-  if(city) {
+  var city = req.query.city;
+  if (city) {
     res.json(timeForOneCity(city));
-  }
-  else{
+  } else {
     res
-    .status(404)
-    .json({Error: "Not a valid endpoint. Please check API Doc"});
+      .status(404)
+      .json({ Error: "Not a valid endpoint. Please check API Doc" });
   }
 });
 
 app.post("/nextFiveHours", (req, res) => {
   let cityDTN = req.body.city_Date_Time_Name;
-  let hours = req.body.hours;  
+  let hours = req.body.hours;
   if (cityDTN && hours) {
-    res.json(nextNhoursWeather(cityDTN, hours, weatherResult))
-  }
-  else{
+    res.json(nextNhoursWeather(cityDTN, hours, weatherResult));
+  } else {
     res
-    .status(404)
-    .json({Error: "Not a valid endpoint. Please check API Doc"})
+      .status(404)
+      .json({ Error: "Not a valid endpoint. Please check API Doc" });
   }
 });
 
 app.listen(Port, (err) => {
-  if(err){
+  if (err) {
     console.log(err);
-  }
-  else{
-    console.log("Server connected at port number 5000 with url http://localhost:5000/");
+  } else {
+    console.log(
+      "Server connected at port number 5000 with url http://localhost:5000/"
+    );
   }
 });
